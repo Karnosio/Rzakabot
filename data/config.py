9ae9afefc7aee0a82b33f1ded8239cf9aeeb876a
Никита Петриченко <petrichenko8502@gmail.com>
2022-07-01 21:18:21 +0300

    edit config

--- data/config.py
+++ data/config.py
@@ -6,8 +6,3 @@ env.read_env()
 VERSION = '0.1'
 API_TOKEN = env.str("BOT_TOKEN")
 ADMIN_ID = env.list("ADMINS")
-
-MySQL_db_name = env.str("MySQL_db_name")
-MySQL_db_login = env.str("MySQL_db_login")
-MySQL_db_password = env.str("MySQL_db_password")
-MySQL_db_host = env.str("MySQL_db_host")
